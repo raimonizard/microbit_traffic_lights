965baf6857486461067ea7ce39f59c12ac7f8e40
raimonizard <97727989+raimonizard@users.noreply.github.com>
2024-06-19 18:39:25 +0200

    chore: pending changes

--- main.py
+++ main.py
@@ -4,23 +4,17 @@ def red_light_on():
         strip.set_pixel_color(1, neopixel.colors(NeoPixelColors.BLACK))
         strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.BLACK))
         strip.show()
-        basic.pause(2000)
-
 def traffic_lights_on():
     while lights_on:
-        basic.show_number(input.light_level())
-            if input.light_level() < 100:
-                set_lights_to_white(3, 255)
-        basic.clear_screen()
         red_light_on()
-        yellow_light_on()
+        basic.pause(2000)
         green_light_on()
-        set_lights_to_black(0, 24)
-        strip.show()
+        basic.pause(2000)
+        yellow_light_on()
+        basic.pause(0)
 
 def on_button_pressed_a():
     global lights_on
-    basic.clear_screen()
     lights_on = True
     traffic_lights_on()
 input.on_button_pressed(Button.A, on_button_pressed_a)
@@ -30,20 +24,12 @@ def set_lights_to_black(first_led: number, last_led: number):
     for i in lights:
         strip.set_pixel_color(i, neopixel.colors(NeoPixelColors.BLACK))
     strip.show()
-
-def set_lights_to_white(first_led: number, last_led: number):
-    lights = range(first_led, last_led)
-    for i in lights:
-        strip.set_pixel_color(i, neopixel.colors(NeoPixelColors.WHITE))
-    strip.show()
-
 def green_light_on():
     if lights_on:
         strip.set_pixel_color(2, neopixel.colors(NeoPixelColors.BLACK))
         strip.set_pixel_color(1, neopixel.colors(NeoPixelColors.BLACK))
         strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.GREEN))
         strip.show()
-        basic.pause(2000)
 
 def on_button_pressed_b():
     global lights_on
@@ -52,22 +38,30 @@ def on_button_pressed_b():
     strip.show()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def set_lights_to_white(first_led2: number, last_led2: number):
+    lights2 = range(first_led2, last_led2)
+    for j in lights2:
+        strip.set_pixel_color(j, neopixel.colors(NeoPixelColors.WHITE))
+    strip.show()
 def yellow_light_on():
     if lights_on:
+        strip.set_pixel_color(1, neopixel.colors(NeoPixelColors.YELLOW))
         strip.set_pixel_color(2, neopixel.colors(NeoPixelColors.BLACK))
         strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.BLACK))
+        strip.show()
+        basic.pause(500)
         blink = 4
         while blink > 0:
+            strip.set_pixel_color(1, neopixel.colors(NeoPixelColors.BLACK))
+            strip.show()
+            basic.pause(500)
             strip.set_pixel_color(1, neopixel.colors(NeoPixelColors.YELLOW))
             strip.show()
             basic.pause(500)
-            strip.set_pixel_color(1, neopixel.colors(NeoPixelColors.BLACK))
-            strip.show()
             blink = blink - 1
-        basic.pause(2000)
-
-
 lights_on = False
 strip: neopixel.Strip = None
+basic.clear_screen()
 strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 set_lights_to_black(0, 24)
+lights_on = False
--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
-function red_light_on () {
+function red_light_on() {
     if (lights_on) {
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
         strip.show()
     }
+    
 }
-function traffic_lights_on () {
+
+function traffic_lights_on() {
     while (lights_on) {
         red_light_on()
         basic.pause(2000)
@@ -16,40 +18,47 @@ function traffic_lights_on () {
         basic.pause(0)
     }
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     lights_on = true
     traffic_lights_on()
 })
-function set_lights_to_black (first_led: number, last_led: number) {
+function set_lights_to_black(first_led: number, last_led: number) {
     let lights = _py.range(first_led, last_led)
-for (let i of lights) {
+    for (let i of lights) {
         strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Black))
     }
     strip.show()
 }
-function green_light_on () {
+
+function green_light_on() {
     if (lights_on) {
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         strip.show()
     }
+    
 }
-input.onButtonPressed(Button.B, function () {
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     lights_on = false
     set_lights_to_black(0, 24)
     strip.show()
 })
-function set_lights_to_white (first_led: number, last_led: number) {
-    let lights2 = _py.range(first_led, last_led)
-for (let j of lights2) {
+function set_lights_to_white(first_led2: number, last_led2: number) {
+    let lights2 = _py.range(first_led2, last_led2)
+    for (let j of lights2) {
         strip.setPixelColor(j, neopixel.colors(NeoPixelColors.White))
     }
     strip.show()
 }
-function yellow_light_on () {
+
+function yellow_light_on() {
     let blink: number;
-if (lights_on) {
+    if (lights_on) {
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
@@ -66,9 +75,11 @@ if (lights_on) {
             blink = blink - 1
         }
     }
+    
 }
+
 let lights_on = false
-let strip: neopixel.Strip = null
+let strip : neopixel.Strip = null
 basic.clearScreen()
 strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 set_lights_to_black(0, 24)
